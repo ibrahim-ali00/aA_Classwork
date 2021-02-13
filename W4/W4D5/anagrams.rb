84396ec5ff5a584bdcb7e27d1df071421c613eae
Anug Saha <anugsaha@gmail.com>
2021-02-12 19:27:39 -0500

    complete phase 2 of anagrams

--- W4/W4D5/anagrams.rb
+++ W4/W4D5/anagrams.rb
@@ -3,16 +3,22 @@ def first_anagram?(str1, str2)
   new_words = letters.permutation(str1.length).to_a
   new_words.map! { |words| words.join("") }
   new_words.include?(str2)
-  #   anagrams = []
-  #   letters.each_index do |i|
-  #     (i + 1...letters.length).each do |j|
-  #       words = str1[i..j]
-  #       anagrams << words
-  #     end
-  #   end
-  #   anagrams.include?(str2)
-  #new_words
 end
 
-p first_anagram?("gizmo", "sally")
-p first_anagram?("elvis", "lives")
+# p first_anagram?("gizmo", "sally")
+# p first_anagram?("elvis", "lives")
+
+def second_anagram?(str1, str2)
+  
+  str1.each_char do |char|
+    # str2.each_char.with_index do |char2, i|
+    str2.slice!(str2.index(char)) if str2.include?(char)  
+  end
+  
+  return str2.empty?
+end
+#iterate over 1st string 
+#find index 
+
+p second_anagram?("elvis", "lives")
+p second_anagram?("gizmo", "sally")
