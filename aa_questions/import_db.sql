d243ffce3f7066eeec75f51286be04438b6c685f
tpadgett <tim_padgett@yahoo.com>
2021-02-17 14:58:01 -0800

    Begin db

--- aa_questions/import_db.sql
+++ aa_questions/import_db.sql
@@ -3,43 +3,63 @@ PRAGMA foreign_keys = ON;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
+    id integer PRIMARY KEY,
     fname TEXT NOT NULL,
     lname TEXT NOT NULL
 );
 
 DROP TABLE IF EXISTS questions;
 CREATE TABLE questions (
+    id integer PRIMARY KEY,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
-
-    FOREIGN KEY (user_lname) references users(lname)
+    user_id INTEGER,
+    FOREIGN KEY (user_id) references users(id)
 );
 
-CREATE TABLE question_follows(
+DROP TABLE IF EXISTS question_follows;
 
+CREATE TABLE question_follows (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    question_id INTEGER
+    --FK for userid and question id
 );
 
-CREATE TABLE replies(
-    REFERENCES subject_question,
-    REFERENCES parent_reply,
-    REFERENCES author
-    body TEXT NOT NULL
+DROP TABLE IF EXISTS replies;
+
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
+    subject TEXT NOT NULL,
+    parent_id INTEGER,
+    question_id INTEGER,
+    user_id INTEGER,
+    body TEXT NOT NULL,
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (parent_id) REFERENCES replies(id)
 );
+
+DROP TABLE IF EXISTS question_likes;
 
 CREATE TABLE question_likes (
-    --Select all: likes?
-    REFERENCES users,
-    REFERENCES questions
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    question_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
 INSERT INTO 
-users ( fname, lname )
+users (fname, lname)
 VALUES
-('John', 'Doe')
-('Jane', 'Doe')
+('John', 'Doe'),
+('Jane', 'Doe');
 
 INSERT INTO
-questions ( title, body)
+questions (title, body)
 VALUES
-('Sequel', 'How to create tables')
-('Ruby', 'How to access tables')
+('Sequel', 'How to create tables'),
+('Ruby', 'How to access tables');
+
+--make sample data for other tables
