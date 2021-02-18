68a9e0d570dc898b0d48f3a7180bf8d22f793a2a
Taylor Musolf <tcmusolf@gmail.com>
2021-02-18 15:16:49 -0800

    Finish associations project

--- associations/app/models/course.rb
+++ associations/app/models/course.rb
@@ -14,4 +14,16 @@ class Course < ApplicationRecord
     foreign_key: :course_id,
     class_name: :Enrollment
 
+    has_many :enrolled_students,
+    through: :enrollments,
+    source: :student
+
+    belongs_to :prerequisite,
+    foreign_key: :prereq_id,
+    class_name: :Course
+    
+    belongs_to :instructor,
+    foreign_key: :instructor_id,
+    class_name: :User
+
 end
--- associations/app/models/user.rb
+++ associations/app/models/user.rb
@@ -11,4 +11,12 @@ class User < ApplicationRecord
     has_many :enrollments,
     foreign_key: :student_id,
     class_name: :Enrollment
+
+    has_many :enrolled_courses,
+    through: :enrollments,
+    source: :course
+
+    has_many :instructed_courses,
+    foreign_key: :instructor_id,
+    class_name: :Course
 end
