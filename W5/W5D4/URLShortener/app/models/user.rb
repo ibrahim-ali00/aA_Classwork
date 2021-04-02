e393b34078d0863169e05d52e13659ca1fb59591
ibrahim-ali00 <77652195+ibrahim-ali00@users.noreply.github.com>
2021-04-02 18:32:49 +0000

    Delete W5 directory

--- W5/W5D4/URLShortener/app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-    validates :email, presence: true, uniqueness: true
-
-    
-  has_many :submitted_urls,
-  class_name: :Shortened_Url,
-  foreign_key: :user_id
-
-end
