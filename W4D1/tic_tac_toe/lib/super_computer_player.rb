ee4ec202711908a11120c82c256ba6f1cdea7656
ibrahim-ali00 <ibrahim.ali00@protonmail.com>
2021-04-05 16:23:49 -0700

    final ttt

--- W4D1/tic_tac_toe/lib/super_computer_player.rb
+++ W4D1/tic_tac_toe/lib/super_computer_player.rb
@@ -2,6 +2,19 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    children = node.children
+    children.each do |child|
+      if child.winning_node?(mark)
+        return child.prev_move_pos
+      end
+    end
+    children.each do |child|
+      if !child.losing_node?(mark)
+        return child.prev_move_pos
+      end
+    end
+    raise "error"
   end
 end
 
--- W4D1/tic_tac_toe/lib/tic_tac_toe_node.rb
+++ W4D1/tic_tac_toe/lib/tic_tac_toe_node.rb
@@ -9,27 +9,28 @@ class TicTacToeNode
     @prev_move_pos = prev_move_pos
   end
 
-  # how do we get opponent turn?
-  # how do we go through each node and check if it is loser or winner?
-  # what is evaluator? a TTT node: board, next_mover, prev_pos
-  # how do we get current mark?
-  def losing_node?(evaluator) #!board.won?
-    #base case: board is over and winner is opponent or winner is nil or us
-    if board.over? 
+  def losing_node?(evaluator)
+    if board.over?
       return board.won? && evaluator != board.winner
     end
 
     if @next_mover_mark == evaluator
-      self.children.all? {|child| child.losing_node?(evaluator)}
-      # losing_node?(evaluator.children.shift)
+      self.children.all? { |child| child.losing_node?(evaluator) }
     else
-      self.children.any? {|child| child.losing_node?(evaluator)}
+      self.children.any? { |child| child.losing_node?(evaluator) }
     end
-    #recursive case: player's turn and all children are losers
-    #OR opponent's turn and one of the childrens nodes is a losing node for the player
   end
 
   def winning_node?(evaluator)
+    if board.over?
+      return board.won? && evaluator == board.winner
+    end
+
+    if @next_mover_mark == evaluator
+      self.children.any? { |child| child.winning_node?(evaluator) }
+    else
+      self.children.all? { |child| child.winning_node?(evaluator) }
+    end
   end
 
   # This method generates an array of all moves that can be made after
@@ -43,7 +44,7 @@ class TicTacToeNode
           duplicate[[i, j]] = next_mover_mark
           opposite = (next_mover_mark == :x ? :o : :x)
           prev_move_pos = [i, j]
-          array << TicTacToeNode.new(duplicate,opposite,prev_move_pos)
+          array << TicTacToeNode.new(duplicate, opposite, prev_move_pos)
         end
       end
     end
