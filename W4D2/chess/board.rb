db8e9e18b9121664d67bb34c7648df957f96826d
ibrahim-ali00 <ibrahim.ali00@protonmail.com>
2021-04-07 11:58:09 -0700

    completed board

--- W4D2/chess/board.rb
+++ W4D2/chess/board.rb
@@ -8,10 +8,6 @@ class Board
     @grid = Array.new(8) { Array.new(8, NullPiece.new) }
   end
 
-  # def move_piece(start, end)
-
-  # end
-
   def [](pos)
     @grid[pos[0]][pos[-1]]
   end
@@ -20,23 +16,29 @@ class Board
     @grid[pos[0]][pos[-1]] = val
   end
 
+  def move_piece(start_pos, end_pos)
+    if self[start_pos].is_a?(NullPiece)
+      raise "Empty position"
+    end
+    end_pos.each do |ele|
+      raise "Out of bounds" if !ele.between?(0, 7)
+    end
+    self[start_pos], self[end_pos] = NullPiece.new, self[start_pos]
+  end
+
   def print_board
     printed = []
     @grid.each do |row|
       row.each do |ele|
-        #printed << ele.symbol
         print ele.symbol
       end
       puts
     end
-    # p printed.each_slice(8).to_a
   end
 end
 
 board = Board.new
-
-
 piece = Piece.new
 board[[0, 0]] = piece
-
+board.move_piece([0, 0], [1, 1])
 board.print_board
