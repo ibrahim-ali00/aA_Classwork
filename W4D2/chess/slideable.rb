c9061551450007c06bc684207e57f23eb21e2d33
ibrahim-ali00 <ibrahim.ali00@protonmail.com>
2021-04-07 17:20:54 -0700

    chess final

--- W4D2/chess/slideable.rb
+++ W4D2/chess/slideable.rb
@@ -1,29 +1,33 @@
-module Slideable 
+module Slideable
+  HORIZONTIAL_VERITCAL = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+  DIAGONAL = [[1, 1], [-1, -1], [1, -1], [-1, 1]]
 
-    HORIZONTIAL_VERITCAL = [[0,1], [0, -1], [1, 0], [-1, 0]]
-    DIAGONAL = [[1, 1], [-1, -1], [1, -1], [-1, 1]]
+  def horizontal_dirs
+    HORIZONTIAL_VERITCAL
+  end
 
-    def horizontal_dirs 
-        
+  def diagonal_dirs
+    DIAGONAL
+  end
+
+  def moves
+    arr
+  end
+
+  private
+
+  def move_dirs
+    
+  end
+
+  def grow_unblocked_moves_in_dir(dx, dy)
+    arr = []
+    move = [self.pos[0] + dx, self.pos[1] + dy]
+    until !self.valid_moves.include?(move)
+        arr << move
+        break if !self.board[move].empty?
+        move = [self.pos[0] + dx, self.pos[1] + dy]
     end
-
-    def diagonal_dirs 
-
-    end
-
-    def moves 
-
-    end
-
-    private 
-
-    def move_dirs 
-
-    end
-
-    def grow_unblocked_moves_in_dir(dx, dy)
-
-    end
-
-
+    arr
+  end
 end
