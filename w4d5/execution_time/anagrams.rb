50407bbd8e92fe221d5e5eb7d07302b7044459da
mohammad <m.a.taghva@gmail.com>
2021-04-09 15:25:39 -0700

    Add Anagram_3,4,5

--- w4d5/execution_time/anagrams.rb
+++ w4d5/execution_time/anagrams.rb
@@ -19,16 +19,50 @@ def second_anagram?(str1, str2)
   arr2.empty?
 end
 
+def third_anagram?(str1, str2)
+
+  str1.split("").sort == str2.split("").sort
+end
+
+def forth_anagram?(str1, str2)
+  hash1 = Hash.new(0)
+  hash2 = Hash.new(0)
+  str1.each_char do |char|
+    hash1[char] += 1
+  end
+  str2.each_char do |char|
+    hash2[char] += 1
+  end
+  hash1 == hash2
+end
+
+
+def fifth_anagram?(str1, str2)
+  hash1 = Hash.new(0)
+  str1.each_char do |char|
+    hash1[char] += 1
+  end
+  str2.each_char do |char|
+    hash1[char] -= 1
+  end
+  hash1.values.any? {|ele| ele != 0}
+end
+
+
+
 # str1 = "basiparachromatin"
 # str2 = "marsipobranchiata"
 
 str1 = "mountaineer"
 str2 = "enumeration"
 
-# p second_anagram?("gizmo", "sally")    #=> false
-# p second_anagram?("elvis", "lives")    #=> true
+# p third_anagram?("elvis", "lives")    #=> true
+# p third_anagram?("gizmo", "sally")    #=> false
 
 Benchmark.bm do |x|
   x.report { first_anagram?(str1, str2) }
   x.report { second_anagram?(str1, str2) }
+  x.report { third_anagram?(str1, str2) }
+  x.report { forth_anagram?(str1, str2) }
+  x.report { fifth_anagram?(str1, str2) }
 end
