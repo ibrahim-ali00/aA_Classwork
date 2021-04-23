a984d1996924f794854a66ab4faa99e71e86ab04
Ying <cy240705@gmail.com>
2021-04-23 13:56:43 -0700

    created age method and validations

--- W6D5/ninety_nine/app/models/cat.rb
+++ W6D5/ninety_nine/app/models/cat.rb
@@ -1,4 +1,25 @@
+require 'action_view'
 class Cat < ApplicationRecord
+    include ActionView::Helpers::DateHelper
+    COLORS =[
+        'black',
+        'white',
+        'orange',
+        'brown',
+        'tabby'
+    ]
+
+    GENDER =[
+        'M',
+        'F'
+    ]
     validates :birth_date, :color, :name, :sex, presence: true
-    validates :color, inclusion:  
+    validates :color, inclusion:  {in: COLORS}
+    validates :sex, inclusion: {in: GENDER}
+
+    def age
+        time_ago_in_words(birth_date)
+    end
+
+    
 end
