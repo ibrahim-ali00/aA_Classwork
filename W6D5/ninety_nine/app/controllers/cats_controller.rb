346b8a42bdb9afd51931498132cebadce8034850
ibrahim-ali00 <ibrahim.ali00@protonmail.com>
2021-04-23 14:58:02 -0700

    added views for new

---
+++ W6D5/ninety_nine/app/controllers/cats_controller.rb
@@ -0,0 +1,16 @@
+class CatsController < ApplicationController
+  def index
+    @cats = Cat.all
+    render :index
+  end
+
+  def show
+    @cats = Cat.find_by(id: params[:id])
+    render :show
+  end
+
+  def new
+    @cat = Cat.new
+    render :new
+  end
+end
