abf63b59f48d81e08fe38dad5dc18d7d468e0c1e
ibrahim-ali00 <ibrahim.ali00@protonmail.com>
2021-04-23 17:01:19 -0700

    views:

--- W6D5/ninety_nine/app/controllers/cats_controller.rb
+++ W6D5/ninety_nine/app/controllers/cats_controller.rb
@@ -13,4 +13,33 @@ class CatsController < ApplicationController
     @cat = Cat.new
     render :new
   end
+
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else
+      render :new
+    end
+
+    def edit
+      @cat = Cat.find_by(id: params[:id])
+      render :edit
+    end
+
+    def update
+      @cat = Cat.find_by(id: params[:id])
+      if @cat.update(cat_params)
+        redirect_to cat_url(@cat)
+      else
+        render :edit
+      end
+    end
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :birth_date, :color, :description, :sex)
+  end
 end
