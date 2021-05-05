6afc2462cd91057e35fc96b1bb28bd0a4c658a06
jon <jonathanyusf@gmail.com>
2021-05-05 16:20:07 -0700

    working on myReduce

--- W8/W8D3/intro_js/phase_2_enumerable.js
+++ W8/W8D3/intro_js/phase_2_enumerable.js
@@ -8,4 +8,44 @@ let callback = function (ele) {
   console.log(ele)
   return ele + ele;
 };
-console.log([1, 2, 3].myEach(callback));
+// console.log([1, 2, 3].myEach(callback));
+
+// Array#myMap(callback) - receives a callback function, returns a new array of the results of calling the callback function on each element of the array
+// should use myEach and a closure
+
+
+Array.prototype.myMap = function (callback) {
+  const arr = []; 
+  
+  this.myEach (function(ele) {
+
+    arr.push(callback(ele));
+  })
+
+  return arr;
+};
+
+// console.log([1, 2, 3].myMap(callback));
+
+// accumulator will be another function 
+
+Array.prototype.myReduce = function(func, value) {
+  let arr = this;
+  if (value === undefined) {
+    value = arr[0] 
+    arr = arr.slice(1)
+    
+  }
+  let result = value
+
+  arr.myEach (function(ele) {
+
+    result = callback(result, ele)
+  })
+
+  return result;
+}
+console.log([1,2,3,4,5].myReduce(function (total, item) {
+  total + item
+}) )
+
