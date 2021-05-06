04157aad176b73c0466300d631913ac6d5a37768
jon <jonathanyusf@gmail.com>
2021-05-05 17:29:32 -0700

    Added Iteration JS problems

--- W8/W8D3/intro_js/phase_3_iteration.js
+++ W8/W8D3/intro_js/phase_3_iteration.js
@@ -13,4 +13,26 @@ Array.prototype.bubbleSort = function () {
   return this;
 };
 
-console.log([5, 7, 3, 2, 4, 43, 34, 1411].bubbleSort());
+// console.log([5, 7, 3, 2, 4, 43, 34, 1411].bubbleSort());
+
+// String#substrings - receives a string, returns an array of all substrings
+
+String.prototype.subStrings = function () {
+
+  const arr = []; 
+
+  for (let i = 0; i < this.length; i++) {
+    arr.push(this[i]);
+    let string = this[i];
+    for (let j = i + 1; j < this.length; j++) {
+      string += this[j];
+      arr.push(string)
+    }
+  }
+
+
+
+  return arr;
+}
+// console.log("cats".subStrings());
+
